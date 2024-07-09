0144f933f763d5a853d4e2043259cdb77f26a249
dmitriyabis <zukovdima0416@gmail.com>
2024-07-10 01:09:44 +0300

    final work

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    backdrop: document.querySelector("[data-backdrop]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.backdrop.classList.toggle("is-hidden");
+    refs.modal.classList.toggle("on-animate");
+  }
+})();
